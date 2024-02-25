22ff7cc7f4f229682538532aba3eba29b26bbdee
eva <evaw0929@gmail.com>
2024-02-25 15:44:49 -0500

    Initial Commit

---
+++ ShoppingListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingListApp
+//
+//  Created by Eva Wang on 2024-02-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingListApp/ShoppingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppApp.swift
+//  ShoppingListApp
+//
+//  Created by Eva Wang on 2024-02-25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
